f3d058ba2341779135bc9ed23019d969fdd5d18a
CarlosReategui <carlos.reategui@utec.edu.pe>
2021-01-17 18:30:03 -0500

    Update loginScreen.dart

--- agricultores_app/lib/screens/loginScreen.dart
+++ agricultores_app/lib/screens/loginScreen.dart
@@ -1,4 +1,4 @@
-import 'package:agricultores_app/screens/registerScreen.dart';
+import 'package:agricultores_app/screens/register/registerScreen.dart';
 import 'package:agricultores_app/services/token.dart';
 import 'package:flutter/material.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
