6e11ff3b5df48bb0c2c634b12ff59ac1ea6abc9e
Christian <christian.ledgard@utec.edu.pe>
2021-01-17 22:28:30 -0500

    registro - codigo/location/photo #5

---
+++ agricultores_app/lib/screens/register/locationRegisterScreen.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show DeviceOrientation, SystemChrome;
+import 'package:location/location.dart';
+import 'package:geocoder/geocoder.dart';
+
+import 'package:flutter_verification_code/flutter_verification_code.dart';
+
+class LocationRegisterScreen extends StatefulWidget {
+  LocationRegisterScreen({Key key}) : super(key: key);
+
+  @override
+  _LocationRegisterScreenState createState() => _LocationRegisterScreenState();
+}
+
+class _LocationRegisterScreenState extends State<LocationRegisterScreen> {
+  String code;
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ],
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registro'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              //Flexible(
+              //child:
+              Image.asset(
+                'assets/images/logo.png',
+                scale: MediaQuery.of(context).size.height /
+                    MediaQuery.of(context).size.width,
+                //),
+              ),
+              Flexible(
+                flex: 3,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  child: Column(
+                    children: [
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                        // onPressed: Token.generateOrRefreshToken(telephone, passwordController.text),
+                        onPressed: () async {
+                          Location location = new Location();
+
+                          bool _serviceEnabled;
+                          PermissionStatus _permissionGranted;
+                          LocationData _locationData;
+
+                          _serviceEnabled = await location.serviceEnabled();
+                          if (!_serviceEnabled) {
+                            _serviceEnabled = await location.requestService();
+                            if (!_serviceEnabled) {
+                              return;
+                            }
+                          }
+
+                          _permissionGranted = await location.hasPermission();
+                          if (_permissionGranted == PermissionStatus.denied) {
+                            _permissionGranted =
+                                await location.requestPermission();
+                            if (_permissionGranted !=
+                                PermissionStatus.granted) {
+                              return;
+                            }
+                          }
+
+                          _locationData = await location.getLocation();
+
+                          print(_locationData.latitude);
+                          print(_locationData.longitude);
+
+                          final coordinates = new Coordinates(
+                            _locationData.latitude,
+                            _locationData.longitude,
+                          );
+                          var addresses = await Geocoder.local
+                              .findAddressesFromCoordinates(coordinates);
+                          var first = addresses.first;
+                          print("${first.toMap()}");
+                        },
+                        color: Colors.green[400],
+                        child: Text('Ubicación GPS',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            )),
+                      ),
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                        // onPressed: Token.generateOrRefreshToken(telephone, passwordController.text),
+                        onPressed: () {
+                          print(code);
+                        },
+                        color: Colors.green[400],
+                        child: Text('Siguiente',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- agricultores_app/lib/screens/register/registerScreen.dart
+++ agricultores_app/lib/screens/register/registerScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show DeviceOrientation, FilteringTextInputFormatter, SystemChrome, TextInputFormatter;
+import 'package:flutter/services.dart' show DeviceOrientation, SystemChrome;
 import 'package:intl_phone_field/intl_phone_field.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -10,7 +10,6 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
-
   final firstNameController = TextEditingController();
   final lastNameController = TextEditingController();
   final dniController = TextEditingController();
@@ -18,8 +17,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final passwordController = TextEditingController();
 
   String telephone;
-
-  static final TextInputFormatter digitsOnly = FilteringTextInputFormatter.allow(RegExp(r'[0-9]'));
 
   @override
   void dispose() {
@@ -31,31 +28,30 @@ class _RegisterScreenState extends State<RegisterScreen> {
     super.dispose();
   }
 
-  Widget textField(TextEditingController controller, String placeholder, bool obscureText, IconData icon, int maxLength) {
-    return Column(
-      children: [
-        TextFormField(
-          controller: controller,
-          obscureText: obscureText,
-          enableSuggestions: false,
-          autocorrect: false,
-          maxLength: maxLength,
-          decoration: InputDecoration(
-            labelText: placeholder,
-            counterText: "",
-            prefixIcon: Icon(
-              icon,
-              color: Colors.green,
-            ),
-            border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.green),
-              borderRadius: const BorderRadius.all(const Radius.circular(20.0)),
-            ),
-          ), 
+  Widget textField(TextEditingController controller, String placeholder,
+      bool obscureText, IconData icon, int maxLength) {
+    return Column(children: [
+      TextFormField(
+        controller: controller,
+        obscureText: obscureText,
+        enableSuggestions: false,
+        autocorrect: false,
+        maxLength: maxLength,
+        decoration: InputDecoration(
+          labelText: placeholder,
+          counterText: "",
+          prefixIcon: Icon(
+            icon,
+            color: Colors.green,
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green),
+            borderRadius: const BorderRadius.all(const Radius.circular(20.0)),
+          ),
         ),
-        SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-      ]
-    );
+      ),
+      SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+    ]);
   }
 
   @override
@@ -70,36 +66,41 @@ class _RegisterScreenState extends State<RegisterScreen> {
         title: Text('Registro'),
       ),
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget> [
-              //Flexible(
-                //child: 
-                Image.asset(
-                  'assets/images/logo.png',
-                  scale: MediaQuery.of(context).size.height / MediaQuery.of(context).size.width,
-                //),
-              ),
-              Flexible(
+          child: SingleChildScrollView(
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+            //Flexible(
+            //child:
+            Image.asset(
+              'assets/images/logo.png',
+              scale: MediaQuery.of(context).size.height /
+                  MediaQuery.of(context).size.width,
+              //),
+            ),
+            Flexible(
                 flex: 3,
                 child: Container(
                   width: MediaQuery.of(context).size.width * 0.8,
                   child: Column(
                     children: [
-                      this.textField(firstNameController, 'Nombres', false, Icons.person, 30),
-                      this.textField(lastNameController, 'Apellidos', false, Icons.person, 30),
-                      this.textField(dniController, 'DNI', false, Icons.call_to_action, 8),
-                      this.textField(rucController, 'RUC', false, Icons.call_to_action_outlined, 10),
+                      this.textField(firstNameController, 'Nombres', false,
+                          Icons.person, 30),
+                      this.textField(lastNameController, 'Apellidos', false,
+                          Icons.person, 30),
+                      this.textField(
+                          dniController, 'DNI', false, Icons.call_to_action, 8),
+                      this.textField(rucController, 'RUC', false,
+                          Icons.call_to_action_outlined, 10),
                       IntlPhoneField(
                         decoration: InputDecoration(
                           labelText: 'Número de celular',
                           border: OutlineInputBorder(
                             borderSide: BorderSide(color: Colors.green),
-                            borderRadius:
-                                const BorderRadius.all(const Radius.circular(20.0)),
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(20.0)),
                           ),
                         ),
                         initialCountryCode: 'PE',
@@ -107,8 +108,10 @@ class _RegisterScreenState extends State<RegisterScreen> {
                           telephone = phone.completeNumber;
                         },
                       ),
-                      SizedBox(height: MediaQuery.of(context).size.height * 0.01),
-                      this.textField(passwordController, 'Contraseña', true, Icons.lock, 4096),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.01),
+                      this.textField(passwordController, 'Contraseña', true,
+                          Icons.lock, 4096),
                       FlatButton(
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(18.0),
@@ -132,12 +135,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       )
                     ],
                   ),
-                )
-              ),
-            ]
-          )
-        )
-      ),
+                )),
+          ]))),
     );
   }
 }
