ae2001680301be07bb5cae1ed44010b6210d9df8
CarlosReategui <carlos.reategui@utec.edu.pe>
2021-02-10 18:38:03 -0500

    agregar lógica para renderizar pantallas para que el usuario termine de registrar su cuenta en caso esta se encuentre incompleta

--- agricultores_app/lib/screens/loginScreen.dart
+++ agricultores_app/lib/screens/loginScreen.dart
@@ -1,7 +1,11 @@
 import 'package:agricultores_app/screens/cultivos/crearCutivoScreen.dart';
 import 'package:agricultores_app/screens/homeScreen.dart';
 import 'package:agricultores_app/screens/orders/createOrderScreen.dart';
+import 'package:agricultores_app/screens/register/codeRegisterScreen.dart';
+import 'package:agricultores_app/screens/register/locationRegisterScreen.dart';
 import 'package:agricultores_app/screens/register/registerScreen.dart';
+import 'package:agricultores_app/screens/register/roleRegisterScreen.dart';
+import 'package:agricultores_app/services/myProfileService.dart';
 import 'package:agricultores_app/services/token.dart';
 import 'package:agricultores_app/widgets/cultivos_orders/unitDropdown.dart';
 import 'package:agricultores_app/widgets/general/cosechaLogo.dart';
@@ -116,10 +120,28 @@ class _LoginScreenState extends State<LoginScreen> {
                   try {
                     await Token.generateTokenFromUserAndPassword(
                         telephone, passwordController.text);
-                    await Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomeScreen()),
-                    );
+                    final user = await MyProfileService.getLoggedinUser();
+                    if (!user.isVerified) {
+                      await Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => CodeRegisterScreen()),
+                      );
+                    } else if (user.ubigeo == "") {
+                      await Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => LocationRegisterScreen()),
+                      );
+                    } else if (user.role == null) {
+                      await Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => RoleRegisterScreen()),
+                      );
+                    } else {
+                      await Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                      );
+                    }
                   } catch (e) {
                     setState(() {
                       this.isLoading = false;
