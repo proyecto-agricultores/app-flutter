aa986605691fe93b618d554046c3a56d0e9852e0
CarlosReategui <carlos.reategui@utec.edu.pe>
2021-02-16 22:07:05 -0500

    Create filterFarmersAndClientsScreen.dart

---
+++ agricultores_app/lib/screens/filters/filterFarmersAndClientsScreen.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:agricultores_app/widgets/location/departmentDropdown.dart';
+import 'package:agricultores_app/widgets/general/divider.dart';
+import 'package:agricultores_app/widgets/general/cosechaGreenButton.dart';
+import 'package:agricultores_app/widgets/general/loading.dart';
+import 'package:agricultores_app/models/regionModel.dart';
+import 'package:flutter/services.dart';
+import 'package:agricultores_app/widgets/cultivos_orders/supplyDropdown.dart';
+import 'package:agricultores_app/services/locationService.dart';
+import 'package:agricultores_app/services/filterService.dart';
+
+class FilterFarmersAndClientsScreen extends StatefulWidget {
+
+  FilterFarmersAndClientsScreen({
+    @required this.title,
+    @required this.role
+  });
+  
+  final title;
+  final role;
+
+  _FilterFarmersAndClientsState createState() => _FilterFarmersAndClientsState(title: this.title, role: this.role);
+
+}
+
+class _FilterFarmersAndClientsState extends State<FilterFarmersAndClientsScreen> {
+
+  _FilterFarmersAndClientsState({
+    @required this.title,
+    @required this.role,
+  });
+
+  final title;
+  final role;
+
+  int _supplyID;
+  int _departmentID;
+  int _regionID;
+  bool _departmentIsSelected = false;
+  bool _fetchingRegions = false;
+  bool _loadingRequest = false;
+  List<Region> _regions = [Region(id: 0, name: '')];
+
+  updateSupplyID(newValue) {
+    setState(() {
+      this._supplyID = newValue;
+    });
+  }
+
+  void _handleDepartmentChange(newValue) {
+    setState(() {
+      this._departmentID = newValue;
+      _departmentIsSelected = true;
+      _fetchingRegions = true;
+      this._regions = [Region(id: 0, name: '')];
+      this._regionID = null;
+      this._getRegions();
+    });
+    this._departmentIsSelected = true;
+  }
+
+  Widget _regionsDropdown() {
+    return IgnorePointer(
+        ignoring: !_departmentIsSelected,
+        child: Container(
+            height: MediaQuery.of(context).size.height * 0.07,
+            width: MediaQuery.of(context).size.height * .8,
+            child: DropdownButtonFormField(
+              isExpanded: true,
+              hint: Text('Seleccione su región'),
+              value: _regionID,
+              validator: (value) => value == null ? 'Campo requerido' : null,
+              onChanged: (newValue) {
+                setState(() {
+                  _regionID = newValue;
+                });
+              },
+              items: this._regions.map((region) {
+                return DropdownMenuItem(
+                  child: new Text(
+                    region.name,
+                    textAlign: TextAlign.center,
+                  ),
+                  value: region.id,
+                );
+              }).toList(),
+            )
+          )
+        );
+  }
+
+  void _getRegions() async {
+    final response =
+        await LocationService.getRegionsByDepartment(this._departmentID);
+    response.insert(0, Region(id: 0, name: 'Todas las regiones'));
+    setState(() {
+      this._regions = response;
+      this._fetchingRegions = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsFlutterBinding.ensureInitialized();
+    SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ],
+    );
+    return Scaffold(
+      appBar: AppBar(title: Text(this.title),),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.1),
+              child: Column(
+                children: [
+                  SupplyDropdown(
+                    supplyID: this._supplyID,
+                    updateSupply: this.updateSupplyID,
+                  ),
+                  CosechaDivider(),
+                  DepartmentDropdown(
+                    onChanged: this._handleDepartmentChange
+                  ),
+                  this._fetchingRegions ? CosechaLoading() : this._regionsDropdown(),
+                  CosechaGreenButton(
+                    isLoading: this._loadingRequest,
+                    text: 'Buscar',
+                    onPressed: () async {
+                      try {
+                        setState(() {
+                          this._loadingRequest = true;
+                        });
+                        var response = await FilterService.filterFarmersAndClients(
+                          this._supplyID,
+                          this._departmentID,
+                          this._regionID,
+                          this.role,
+                        );
+                        print(response);
+                        setState(() {
+                          this._loadingRequest = false;
+                        });
+                      } catch (e) {
+                        print(e.toString());
+                      }
+                    },
+                  )
+                ]
+              )
+            )
+          )
+        ]
+      )
+    );
+  }
+
+}
