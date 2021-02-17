ebff87d5ee8e25370be4daa513d11fd139df1178
CarlosReategui <carlos.reategui@utec.edu.pe>
2021-02-16 22:07:50 -0500

    agregar filter de agricultores y clientes

--- agricultores_app/lib/services/filterService.dart
+++ agricultores_app/lib/services/filterService.dart
@@ -2,11 +2,12 @@ import 'package:agricultores_app/global/myHTTPConnection.dart';
 import 'package:agricultores_app/interceptor/httpInterceptor.dart';
 import 'package:agricultores_app/models/myPubModel.dart';
 import 'package:agricultores_app/models/myOrderModel.dart';
+import 'package:agricultores_app/models/myUserModel.dart';
 import 'dart:convert';
 
 class FilterService {
 
-  static filter(int supplyID, int departmentID, int regionID, String minPrice, String maxPrice, String minHarvestDate, String maxHarvestDate, String role) async {
+  static filterPubsAndOrders(int supplyID, int departmentID, int regionID, String minPrice, String maxPrice, String minHarvestDate, String maxHarvestDate, String role) async {
     String suffix = 'api/filter/';
 
     role == 'co' ? suffix += 'orders/?' : suffix += 'pubs/?';
@@ -39,7 +40,41 @@ class FilterService {
       }
       return results;
     } else {
-      throw Exception('Error al intentar aplicar el filtro');
+      throw Exception('Error al intentar aplicar el filtro de ' + role == 'co' ? 'órdenes' : 'publicaciones');
+    }
+  }
+
+  static filterFarmersAndClients(int supplyID, int departmentID, int regionID, String role) async {
+    String suffix = 'api/filter/';
+
+    role == 'co' ? suffix += 'compradores/?' : suffix += 'agricultores/?';
+
+    Map<String, String> queryParams = {};
+
+    print('supplyID: ' + supplyID.toString());
+
+    if (supplyID != null) queryParams['supply'] = supplyID.toString();
+    if (departmentID != null) queryParams['department'] = departmentID.toString();
+    if (regionID != 0 && regionID != null) queryParams['region'] = regionID.toString();
+
+    String queryString = Uri(queryParameters: queryParams).query;
+
+    suffix += queryString;
+
+    final response = await HTTPClient.getClient(WithToken.yes).get(
+      MyHTTPConection.HTTP_URL + suffix,
+    );
+
+    if (response.statusCode == 200) {
+      var results = <MyUser>[];
+      var json = jsonDecode(utf8.decode(response.bodyBytes));
+      for (var result in json) {
+        var v = MyUser.fromJson(result);
+        results.add(v);
+      }
+      return results;
+    } else {
+      throw Exception('Error al intentar aplicar el filtro de ' + role == 'co' ? 'compradores' : 'agricultores');
     }
   }
 
