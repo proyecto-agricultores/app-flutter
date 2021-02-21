2260fb6ddb9acccdc36728fdb8bbc77e195e3b72
CarlosReategui <carlos.reategui@utec.edu.pe>
2021-02-20 20:57:54 -0500

    refactor de dropdown

--- agricultores_app/lib/widgets/location/departmentDropdown.dart
+++ agricultores_app/lib/widgets/location/departmentDropdown.dart
@@ -1,7 +1,7 @@
 import 'package:agricultores_app/models/departmentModel.dart';
 import 'package:agricultores_app/services/locationService.dart';
+import 'package:agricultores_app/widgets/location/locationDropdown.dart';
 import 'package:flutter/material.dart';
-import 'package:agricultores_app/widgets/general/loading.dart';
 
 class DepartmentDropdown extends StatefulWidget {
   DepartmentDropdown({this.onChanged, this.selectedDepartment});
@@ -33,27 +33,12 @@ class _DepartmentDropdownState extends State<DepartmentDropdown> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.11,
-      width: MediaQuery.of(context).size.width * 0.8,
-      child: this._fetchingDepartments 
-      ? CosechaLoading()
-      : DropdownButtonFormField(
-        validator: (value) => value == null ? 'Campo requerido' : null,
-        isExpanded: true,
-        hint: Text('Seleccione su departamento'),
-        value: this.widget.selectedDepartment,
-        onChanged: this.widget.onChanged,
-        items: this._departments.map((department) {
-          return DropdownMenuItem(
-            child: new Text(
-              department.name,
-              textAlign: TextAlign.center,
-            ),
-            value: department.id,
-          );
-        }).toList(),
-      ),
+    return LocationDropdown(
+      ignoreCondition: false,
+      isLoading: this._fetchingDepartments,
+      selectedLocation: this.widget.selectedDepartment,
+      onChanged: this.widget.onChanged,
+      listItems: this._departments,
     );
   }
   
