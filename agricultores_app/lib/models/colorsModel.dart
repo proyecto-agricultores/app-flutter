af2f5ec87c91c3ba6392ebec6eec7688eb19b42a
CarlosReategui <carlos.reategui@utec.edu.pe>
2021-02-20 23:04:22 -0500

    Create colorsModel.dart

---
+++ agricultores_app/lib/models/colorsModel.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CosechaColors {
+
+  final Color naranjaFuerte = Color(0xfffc6e08);
+  final Color naranjaSuave = Color(0xffffcc99);
+  final Color verdeFuerte = Color(0xff09b44d);
+  final Color verdeSuave = Color(0xffd0f1dd);
+
+  Color getNaranjaFuerte() {
+    return naranjaFuerte;
+  }
+
+  Color getNaranjaSuave() {
+    return naranjaSuave;
+  }
+
+  Color getVerdeFuerte() {
+    return verdeFuerte;
+  }
+
+  Color getVerdeSuave() {
+    return verdeSuave;
+  }
+
+}
