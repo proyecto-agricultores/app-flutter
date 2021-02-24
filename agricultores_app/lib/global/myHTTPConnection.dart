60ce8ad8a4e4c372197d81173fc68ece8fe8a5d9
CarlosReategui <carlos.reategui@utec.edu.pe>
2021-02-24 17:27:40 -0500

    cambiar ruta al backend

--- agricultores_app/lib/global/myHTTPConnection.dart
+++ agricultores_app/lib/global/myHTTPConnection.dart
@@ -1,3 +1,3 @@
 class MyHTTPConection {
-  static const String HTTP_URL = 'https://dev-cosecha-pr-30.herokuapp.com/';
+  static const String HTTP_URL = 'https://dev-cosecha-pr-32.herokuapp.com/';
 }
