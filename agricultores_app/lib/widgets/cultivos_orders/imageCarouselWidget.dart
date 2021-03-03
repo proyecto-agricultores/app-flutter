47d01b41e75df684c133e6707d3759eb8ec3094f
Christian <christian.ledgard@utec.edu.pe>
2021-03-03 12:24:45 -0500

    editar fotos en crear cultivo screen

--- agricultores_app/lib/widgets/cultivos_orders/imageCarouselWidget.dart
+++ agricultores_app/lib/widgets/cultivos_orders/imageCarouselWidget.dart
@@ -3,42 +3,76 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-class ImageCarousel extends StatelessWidget{
-  ImageCarousel({this.images, this.getImage});
+class ImageCarousel extends StatefulWidget {
+  final List<File> images;
+  final getImage;
+
+  const ImageCarousel({
+    Key key,
+    @required this.images,
+    @required this.getImage,
+  }) : super(key: key);
+
+  @override
+  ImageCarouselState createState() => ImageCarouselState(images, getImage);
+}
+
+class ImageCarouselState extends State<ImageCarousel> {
   final List<File> images;
   final getImage;
   final picker = ImagePicker();
+  ImageCarouselState(this.images, this.getImage);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        images.isNotEmpty 
-          ? Container(
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  height: 200.0,
-                  viewportFraction: 0.6,
-                  enableInfiniteScroll: false,
-                  enlargeCenterPage: true,
-                ),
-                items: images
-                    .map(
-                      (item) => Container(
-                        height: 500.0,
-                        decoration: new BoxDecoration(
-                          color: const Color(0xff7c94b6),
-                          image: DecorationImage(
-                            image: new FileImage(item),
-                            fit: BoxFit.cover,
+        images.isNotEmpty
+            ? Container(
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: 200.0,
+                    viewportFraction: 0.6,
+                    enableInfiniteScroll: false,
+                    enlargeCenterPage: true,
+                  ),
+                  items: images.map((item) {
+                    return Stack(
+                      children: [
+                        Container(
+                          height: 500.0,
+                          decoration: new BoxDecoration(
+                            color: const Color(0xff7c94b6),
+                            image: DecorationImage(
+                              image: new FileImage(item),
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                      ),
-                    )
-                    .toList(),
-              ),
-            )
-          : Container(),
+                        Positioned(
+                          top: 0,
+                          right: -20,
+                          child: RawMaterialButton(
+                            elevation: 2.0,
+                            fillColor: Colors.red,
+                            child: Icon(
+                              Icons.close,
+                              size: 25.0,
+                              color: Colors.white,
+                            ),
+                            shape: CircleBorder(),
+                            onPressed: () {
+                              images.remove(item);
+                              setState(() {});
+                            },
+                          ),
+                        ),
+                      ],
+                    );
+                  }).toList(),
+                ),
+              )
+            : Container(),
         SizedBox(height: 30),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
