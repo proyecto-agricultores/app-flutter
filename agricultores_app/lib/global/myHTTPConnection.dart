1d0336923938a7bb4f8fd965431456d36f2bc114
CarlosReategui <carlos.reategui@utec.edu.pe>
2021-05-11 14:01:26 -0500

    cambiar api url

--- agricultores_app/lib/global/myHTTPConnection.dart
+++ agricultores_app/lib/global/myHTTPConnection.dart
@@ -1,4 +1,4 @@
 class MyHTTPConection {
-  // static const String HTTP_URL = 'https://cosecha-api.herokuapp.com/';
-  static const String HTTP_URL = 'http://127.0.0.1:8000/';
+  static const String HTTP_URL = 'https://cosecha-api.herokuapp.com/';
+  // static const String HTTP_URL = 'http://127.0.0.1:8000/';
 }
